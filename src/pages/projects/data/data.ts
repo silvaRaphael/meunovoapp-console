6e439b1e38f3fba2cd2104617fc840b8651f2b35
silvaRaphael <raphaeltiago02@gmail.com>
2023-10-25 12:05:31 -0300

    calendar

--- src/pages/projects/data/data.ts
+++ src/pages/projects/data/data.ts
@@ -4,9 +4,10 @@ import { members } from "../../members/data/data";
 import { teams } from "../../teams/data/data";
 import { statuses } from "./status";
 import { Priority, priorities } from "./priority";
+import { addDays } from "date-fns";
 // import { tasks } from "../../tasks/data/data";
 
-export const projects: Project[] = Array.from(new Array(8)).map(() => {
+export const projects: Project[] = Array.from(new Array(20)).map(() => {
     const teamStartIndex = faker.number.int({ min: 0, max: teams.length - 3 });
     // const taskStartIndex = faker.number.int({ min: 0, max: tasks.length - 4 });
     const item: Project = {
@@ -18,7 +19,11 @@ export const projects: Project[] = Array.from(new Array(8)).map(() => {
         // tasks: tasks.slice(taskStartIndex, taskStartIndex + faker.number.int({ min: 1, max: 4 })),
         priority: faker.number.int({ min: 0, max: priorities.length - 1 }) as Priority,
         status: statuses[faker.number.int({ min: 0, max: statuses.length - 1 })],
+        start: new Date(),
         due: faker.date.anytime(),
     };
-    return item;
+    return {
+        ...item,
+        start: addDays(item.due, -60),
+    };
 });
--- src/pages/projects/data/project.ts
+++ src/pages/projects/data/project.ts
@@ -13,5 +13,6 @@ export interface Project {
     // tasks: Task[];
     priority: Priority;
     status: Status;
+    start: Date;
     due: Date;
 }
