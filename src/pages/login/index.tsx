d1c83464c735b0b8a682ba7f115a44a734711dec
silvaRaphael <raphaeltiago02@gmail.com>
2023-11-24 06:07:28 -0300

    fix: login logo

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -9,7 +9,7 @@ export function Login() {
         <div className="flex min-h-screen items-center w-full">
             <Section size="tiny" className="w-full space-y-10">
                 <div className="space-y-3 w-full">
-                    <Logo className="mx-auto w-min -translate-x-4" />
+                    <Logo className="justify-center" />
                     <h4 className="text-muted-foreground leading-5 text-center mx-auto">Acessar o console</h4>
                 </div>
                 <LoginInForm />
