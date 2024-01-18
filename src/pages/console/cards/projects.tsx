782c742f637fe7114e733bbcd4449e8362b1a599
silvaRaphael <raphaeltiago02@gmail.com>
2024-01-18 08:53:33 -0300

    fix: dashboard projects

--- src/pages/console/cards/projects.tsx
+++ src/pages/console/cards/projects.tsx
@@ -2,40 +2,33 @@ import { Langs } from "config/languages";
 import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from "recharts";
 
 export function ProjectsCard({
-    projects,
-    writeLang,
+  projects,
+  writeLang,
 }: {
-    projects: {
-        name: string;
-        progress: number;
-    }[];
-    writeLang: (texts: [Langs, React.ReactNode][]) => React.ReactNode;
+  projects: {
+    name: string;
+    progress: number;
+  }[];
+  writeLang: (texts: [Langs, React.ReactNode][]) => React.ReactNode;
 }) {
-    return (
-        <ResponsiveContainer width="100%" height={200}>
-            {projects.length ? (
-                <BarChart layout="vertical" data={projects}>
-                    <XAxis type="number" tickLine={false} axisLine={false} tickFormatter={(value) => `${value}%`} />
-                    <YAxis
-                        dataKey="name"
-                        type="category"
-                        tickLine={false}
-                        axisLine={false}
-                        tickMargin={20}
-                        width={120}
-                    />
-                    <Bar dataKey="progress" fill="currentColor" radius={[0, 4, 4, 0]} className="fill-primary" />
-                </BarChart>
-            ) : (
-                <div className="flex w-full h-full justify-center items-center">
-                    <span className="font-medium text-muted-foreground">
-                        {writeLang([
-                            ["en", "No projects yet."],
-                            ["pt", "Nenhum projeto ainda."],
-                        ])}
-                    </span>
-                </div>
-            )}
-        </ResponsiveContainer>
-    );
+  return (
+    <ResponsiveContainer width="100%" height={200}>
+      {projects.length ? (
+        <BarChart layout="vertical" data={projects}>
+          <XAxis type="number" tickLine={false} axisLine={false} tickFormatter={(value) => `${value}%`} />
+          <YAxis dataKey="name" type="category" tickLine={false} axisLine={false} tickMargin={20} width={120} />
+          <Bar dataKey="progress" fill="currentColor" radius={[0, 4, 4, 0]} className="fill-primary" />
+        </BarChart>
+      ) : (
+        <div className="flex w-full h-full justify-center items-center">
+          <span className="font-medium text-muted-foreground">
+            {writeLang([
+              ["en", "No projects yet."],
+              ["pt", "Nenhum projeto ainda."],
+            ])}
+          </span>
+        </div>
+      )}
+    </ResponsiveContainer>
+  );
 }
--- src/pages/console/index.tsx
+++ src/pages/console/index.tsx
@@ -16,157 +16,149 @@ import { PreferencesSchema } from "adapters/preferences";
 import { Loader } from "lucide-react";
 
 export function Console() {
-    const { language, writeLang } = useLanguage();
+  const { language, writeLang } = useLanguage();
 
-    const [isLoading, setIsloading] = useState<boolean>(true);
-    const [projects, setProjects] = useState<
-        {
-            name: string;
-            progress: number;
-            due: Date;
-        }[]
-    >([]);
-    const [users, setUsers] = useState<
-        {
-            name: string;
-            email: string;
-            avatar: string;
-            activated_at: Date;
-            is_manager: boolean;
-        }[]
-    >([]);
-    const [preferences, setPreferences] = useState<PreferencesSchema>();
+  const [isLoading, setIsloading] = useState<boolean>(true);
+  const [projects, setProjects] = useState<
+    {
+      name: string;
+      progress: number;
+      due: Date;
+    }[]
+  >([]);
+  const [users, setUsers] = useState<
+    {
+      name: string;
+      email: string;
+      avatar: string;
+      activated_at: Date;
+      is_manager: boolean;
+    }[]
+  >([]);
+  const [preferences, setPreferences] = useState<PreferencesSchema>();
 
-    async function getProjects() {
-        const request = await new HandleRequest().get(`/dashboard/projects`, { language });
+  async function getProjects() {
+    const request = await new HandleRequest().get(`/dashboard/projects`, { language });
 
-        request.onDone((response) => {
-            setProjects(response);
-        });
+    request.onDone((response) => {
+      setProjects(response);
+    });
 
-        request.onError((error) => {
-            errorToast(error);
-        });
-    }
+    request.onError((error) => {
+      errorToast(error);
+    });
+  }
 
-    async function getUsers() {
-        const request = await new HandleRequest().get(`/dashboard/users`, { language });
+  async function getUsers() {
+    const request = await new HandleRequest().get(`/dashboard/users`, { language });
 
-        request.onDone((response) => {
-            setUsers(response);
-        });
+    request.onDone((response) => {
+      setUsers(response);
+    });
 
-        request.onError((error) => {
-            errorToast(error);
-        });
-    }
+    request.onError((error) => {
+      errorToast(error);
+    });
+  }
 
-    async function getPreferences() {
-        const request = await new HandleRequest().get(`/preferences`, { language });
+  async function getPreferences() {
+    const request = await new HandleRequest().get(`/preferences`, { language });
 
-        request.onDone((response) => {
-            setPreferences(response);
-        });
+    request.onDone((response) => {
+      setPreferences(response);
+    });
 
-        request.onError((error) => {
-            errorToast(error);
-        });
-    }
+    request.onError((error) => {
+      errorToast(error);
+    });
+  }
 
-    useEffect(() => {
-        const controller = new AbortController();
+  useEffect(() => {
+    const controller = new AbortController();
 
-        (async () => {
-            await Promise.all([getProjects(), getUsers(), getPreferences()]);
-            setIsloading(false);
-        })();
+    (async () => {
+      await Promise.all([getProjects(), getUsers(), getPreferences()]);
+      setIsloading(false);
+    })();
 
-        return () => {
-            controller.abort();
-        };
+    return () => {
+      controller.abort();
+    };
 
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-    const Loading = () => (
-        <div className="flex w-full h-[200px] justify-center items-center">
-            <Loader size={16} className="animate-spin" />
+  const Loading = () => (
+    <div className="flex w-full h-[200px] justify-center items-center">
+      <Loader size={16} className="animate-spin" />
+    </div>
+  );
+
+  return (
+    <Page pathname="/" header={<SectionHeader isRoot title={`Console`} pathname="/"></SectionHeader>}>
+      <div className="space-y-8">
+        <div className="grid grid-cols-12 gap-4">
+          <Card className="lg:col-span-7 sm:col-span-6 col-span-12">
+            <CardHeader>
+              <CardTitle>
+                {writeLang([
+                  ["en", "Projects"],
+                  ["pt", "Projetos"],
+                ])}
+              </CardTitle>
+            </CardHeader>
+            <CardContent>
+              {isLoading ? <Loading /> : <ProjectsCard projects={projects} writeLang={writeLang} />}
+            </CardContent>
+          </Card>
+          <Card className="lg:col-span-5 sm:col-span-6 col-span-12">
+            <CardHeader>
+              <CardTitle>
+                {writeLang([
+                  ["en", "Users"],
+                  ["pt", "Usuários"],
+                ])}
+              </CardTitle>
+            </CardHeader>
+            <CardContent>
+              {isLoading ? <Loading /> : <UsersCard users={users} language={language} writeLang={writeLang} />}
+            </CardContent>
+          </Card>
         </div>
-    );
-
-    return (
-        <Page pathname="/" header={<SectionHeader isRoot title={`Console`} pathname="/"></SectionHeader>}>
-            <div className="space-y-8">
-                <div className="grid grid-cols-12 gap-4">
-                    <Card className="lg:col-span-7 sm:col-span-6 col-span-12">
-                        <CardHeader>
-                            <CardTitle>
-                                {writeLang([
-                                    ["en", "Projects"],
-                                    ["pt", "Projetos"],
-                                ])}
-                            </CardTitle>
-                        </CardHeader>
-                        <CardContent>
-                            {isLoading ? <Loading /> : <ProjectsCard projects={projects} writeLang={writeLang} />}
-                        </CardContent>
-                    </Card>
-                    <Card className="lg:col-span-5 sm:col-span-6 col-span-12">
-                        <CardHeader>
-                            <CardTitle>
-                                {writeLang([
-                                    ["en", "Users"],
-                                    ["pt", "Usuários"],
-                                ])}
-                            </CardTitle>
-                        </CardHeader>
-                        <CardContent>
-                            {isLoading ? (
-                                <Loading />
-                            ) : (
-                                <UsersCard users={users} language={language} writeLang={writeLang} />
-                            )}
-                        </CardContent>
-                    </Card>
-                </div>
-                <div className="flex flex-wrap gap-4">
-                    <Card>
-                        <CardContent className="pt-2">
-                            <Calendar
-                                locale={languages.find((item) => item.lang === language.lang)?.dateLocale}
-                                mode="multiple"
-                                fromMonth={new Date()}
-                                toMonth={addMonths(new Date(), 12)}
-                                numberOfMonths={2}
-                                selected={projects.map((item) => new Date(item.due))}
-                            />
-                        </CardContent>
-                    </Card>
-                    <Card className="h-full">
-                        <CardHeader>
-                            <CardTitle>
-                                {writeLang([
-                                    ["en", "Notifications"],
-                                    ["pt", "Notificações"],
-                                ])}
-                            </CardTitle>
-                            <CardDescription>
-                                {writeLang([
-                                    ["en", "Change your notification preferences"],
-                                    ["pt", "Altere suas preferências de notificações"],
-                                ])}
-                            </CardDescription>
-                        </CardHeader>
-                        <CardContent>
-                            {isLoading || !preferences ? (
-                                <Loading />
-                            ) : (
-                                <NotificationsPreferencesForm preferences={preferences} />
-                            )}
-                        </CardContent>
-                    </Card>
-                </div>
-            </div>
-        </Page>
-    );
+        <div className="flex flex-wrap gap-4">
+          <Card>
+            <CardContent className="pt-2">
+              <Calendar
+                locale={languages.find((item) => item.lang === language.lang)?.dateLocale}
+                mode="multiple"
+                fromMonth={new Date()}
+                toMonth={addMonths(new Date(), 12)}
+                numberOfMonths={2}
+                selected={projects.map((item) => new Date(item.due))}
+              />
+            </CardContent>
+          </Card>
+          <Card className="h-full">
+            <CardHeader>
+              <CardTitle>
+                {writeLang([
+                  ["en", "Notifications"],
+                  ["pt", "Notificações"],
+                ])}
+              </CardTitle>
+              <CardDescription>
+                {writeLang([
+                  ["en", "Change your notification preferences"],
+                  ["pt", "Altere suas preferências de notificações"],
+                ])}
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              {isLoading || !preferences ? <Loading /> : <NotificationsPreferencesForm preferences={preferences} />}
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+    </Page>
+  );
 }
