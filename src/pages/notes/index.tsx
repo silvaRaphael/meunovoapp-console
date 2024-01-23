ecf0f76d2176fa74e4d5041a016ee88f1d74d750
silvaRaphael <raphaeltiago02@gmail.com>
2024-01-23 19:55:24 -0300

    add: note delete

--- src/pages/notes/index.tsx
+++ src/pages/notes/index.tsx
@@ -8,17 +8,35 @@ import { Note } from "./data/note";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "components/ui/card";
 import { Badge } from "components/ui/badge";
 import { CreateNoteForm } from "./forms/create";
+import { ConfirmationAlert } from "components/shared/confirmation-alert";
+import { Button, buttonVariants } from "components/ui/button";
+import { Trash2 } from "lucide-react";
+import { SubmitButton } from "components/shared/submit-button";
 
 export function Notes() {
   const { language, writeLang } = useLanguage();
 
   const [notes, setNotes] = useState<Note[]>([]);
+  const [deleteIsOpen, setDeleteIsOpen] = useState<boolean>(false);
 
   async function getNotes() {
     const request = await new HandleRequest().get(`/notes`, { language });
 
     request.onDone((response) => {
       setNotes(response);
+    });
+
+    request.onError((error) => {
+      errorToast(error);
+    });
+  }
+
+  async function handleDelete(id: string) {
+    const request = await new HandleRequest().delete(`/notes/${id}`, { language });
+
+    request.onDone(() => {
+      setNotes(notes.filter((item) => item.id !== id));
+      setDeleteIsOpen(false);
     });
 
     request.onError((error) => {
@@ -63,7 +81,43 @@ export function Notes() {
         {notes.map((item, i) => (
           <Card key={i} className="h-min">
             <CardHeader>
-              <CardTitle>{item.title}</CardTitle>
+              <CardTitle className="flex justify-between">
+                {item.title}
+                <ConfirmationAlert
+                  title={
+                    writeLang([
+                      ["en", "Delete this Note"],
+                      ["pt", "Excluir esta Nota"],
+                    ]) as string
+                  }
+                  description={
+                    writeLang([
+                      ["en", "This action cannot be undone!"],
+                      ["pt", "Esta ação não poderá ser desfeita!"],
+                    ]) as string
+                  }
+                  triggerButton={
+                    <Button variant="ghost" size="sm" className="text-muted-foreground hover:text-primary">
+                      <Trash2 size={14} />
+                    </Button>
+                  }
+                  confirmButton={
+                    <SubmitButton
+                      type="button"
+                      onSubmit={() => handleDelete(item.id)}
+                      className={buttonVariants({ variant: "destructive" })}
+                      label={
+                        writeLang([
+                          ["en", "Delete Note"],
+                          ["pt", "Excluir Nota"],
+                        ]) as string
+                      }
+                    />
+                  }
+                  open={deleteIsOpen}
+                  onOpenChange={setDeleteIsOpen}
+                />
+              </CardTitle>
             </CardHeader>
             <CardContent className="text-start space-y-4">
               <CardDescription>{item.content}</CardDescription>
